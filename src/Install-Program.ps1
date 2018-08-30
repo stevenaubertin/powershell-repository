cf910701e86d6f7a714deefc98ab4f91b2ff502a
Steven Aubertin <stevenaubertin@gmail.com>
2018-08-30 00:40:52 -0400

    Added a script that install program using chocolatey

---
+++ src/Install-Program.ps1
@@ -0,0 +1,56 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 1da94b33-7d2b-4a81-a3b2-acc93972335c
+
+.AUTHOR stevenaubertin@gmail.com
+
+.COMPANYNAME 
+
+.COPYRIGHT (c) 2018
+
+.TAGS 
+
+.LICENSEURI https://github.com/stevenaubertin/powershell-repository/blob/master/LICENSE.md
+
+.PROJECTURI https://github.com/stevenaubertin/powershell-repository
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+.PRIVATEDATA 
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Install a program 
+
+#> 
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [string]$Name
+)
+
+try{
+    chocos install $Name
+}
+catch{    
+    $install = 'n'
+    while(!('y','n' -contains ($install = $(Read-Host "To install $Name this script requires chocolatey which is not found, would you like to install it ?([Y]es/[N]o)")))){}
+    if('y' -eq $install){
+        [System.Diagnostics.Process]::Start("https://chocolatey.org/install")
+    }
+}
